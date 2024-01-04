56f22295fa07be41c44716f94bcaa5fefe39b870
mayarramr <mayaramrsayedhassan@gmail.com>
2024-01-04 19:16:09 +0200

    sixth commit

--- src/Components/Layout/Navbar/Navbar.jsx
+++ src/Components/Layout/Navbar/Navbar.jsx
@@ -34,7 +34,7 @@ export default function Navbar() {
     
     <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
   <div className="container">
-    <Link className="navbar-brand" to="/">
+    <Link className="navbar-brand" to="/home">
         <img src={logo} alt="fresh-market-logo" />
     </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
